df84928b876a4b67de1f6757bf11168d061a317c
NinaShvets <shvetsnina@icloud.com>
2023-07-05 23:37:29 +0300

    hw8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,29 @@
 import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('iframe');
+console.log(iframe);
+const player = new Player(iframe);
+console.log(player);
+player.on('timeupdate', throttle(onPlay, 1000));
+function onPlay({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(seconds));
+}
+const localS = localStorage.getItem('videoplayer-current-time');
+player
+  .setCurrentTime(localS)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
 console.log(Player);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+const throttle = require('lodash.throttle');
+const form = document.querySelector('.feedback-form');
 
+const saveFormState = throttle(
+  () => {
+    const email = form.querySelector('input[name="email"]').value;
+    const message = form.querySelector('textarea[name="message"]').value;
+    const formState = { email, message };
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+  },
+  500,
+  { leading: true, trailing: true }
+);
+
+form.addEventListener('input', saveFormState);
+
+const loadFormState = () => {
+  const storedState = localStorage.getItem('feedback-form-state');
+
+  if (storedState) {
+    const { email, message } = JSON.parse(storedState);
+    form.querySelector('input[name="email"]').value = email;
+    form.querySelector('textarea[name="message"]').value = message;
+  }
+};
+
+window.addEventListener('load', loadFormState);
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const email = form.querySelector('input[name="email"]').value;
+  const message = form.querySelector('textarea[name="message"]').value;
+
+  const formData = { email, message };
+
+  console.log(formData);
+
+  localStorage.removeItem('feedback-form-state');
+  form.reset();
+});
